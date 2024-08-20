8e5aa434a695f13b007bfc9a17f12903fbcf8059
VEDANT MOHOL <152087070+vedantmohol@users.noreply.github.com>
2024-08-20 17:24:21 +0530

    Update Navbar.jsx again

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,19 +1,19 @@
 import { NavLink } from "react-router-dom"
 import { MdCategory, MdContacts, MdHomeFilled, MdShop2 } from "react-icons/md"
 
-const Navbar = ({ containerStyles }) => {
+const Navbar = ({ containerStyles, onItemClick }) => {
     return (
         <nav className={`${containerStyles}`}>
-            <NavLink to={'/'} className={({isActive}) => isActive ? "active_link" : ""}>
+            <NavLink onClick={onItemClick} to={'/'} className={({isActive}) => isActive ? "active_link" : ""}>
                 <div className="flexCenter gap-x-1"><MdHomeFilled />Home</div>
             </NavLink>
-            <NavLink to={'/aboutus'} className={({isActive}) => isActive ? "active_link": ""}>
+            <NavLink onClick={onItemClick} to={'/aboutus'} className={({isActive}) => isActive ? "active_link": ""}>
                 <div className="flexCenter gap-x-1"><MdCategory />About Us</div>
             </NavLink>
-            <NavLink to={'/products'} className={({isActive}) => isActive ? "active_link": ""}>
+            <NavLink onClick={onItemClick} to={'/products'} className={({isActive}) => isActive ? "active_link": ""}>
                 <div className="flexCenter gap-x-1"><MdShop2 />Products</div>
             </NavLink>
-            <NavLink to={'/enquiry'} className={({isActive}) => isActive ? "active_link": ""}>
+            <NavLink onClick={onItemClick} to={'/enquiry'} className={({isActive}) => isActive ? "active_link": ""}>
                 <div className="flexCenter gap-x-1"><MdContacts/>Enquiry</div>
             </NavLink>
         </nav>
